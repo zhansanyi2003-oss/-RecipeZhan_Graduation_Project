41a25fbbef96dd3b59ffb4657fccac96e3c789f5
zhans <zhansanyi2003@gmail.com>
2026-02-17 12:05:01 +0100

    Project initialized

---
+++ Recipe_BackEnd/src/main/java/org/zhan/recipe_backend/RecipeBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.zhan.recipe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBackEndApplication.class, args);
+	}
+
+}
